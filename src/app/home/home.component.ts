cbd2ba6a9aa846167a4f35c7ff985dfe9af0dfe7
Ishani Aggarwal <ishaniaggarwal123@gmail.com>
2019-09-29 11:40:46 +0530

    bootstrap coding

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  // selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+public srcURL1= '../../assets/images/friends.png';
+public srcURL2= '../../assets/images/Got.jpg';
+public srcURL3= '../../assets/images/Sacredgames.png';
+  constructor() { }
+
+  ngOnInit() {
+
+    
+    }
+
+}
